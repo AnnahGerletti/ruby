de271fb8921ec3ca53757dff8a4a570e8f131d79
AnnahGerletti <agerletti@gmail.com>
2018-02-02 09:19:09 +1300

    puts

---
+++ fundimentals.rb
@@ -0,0 +1,10 @@
+#Ruby is a language like JavaScript
+
+# [puts - put string] outputs text to the screen like console.log, will alwasy have a line break after each line.
+puts "hello world"
+puts "uauau" 
+puts 5 #will output the number no double quotes 
+puts 5 + 4 #will output 9 the + concatinates the two numbers 
+puts "3" + "2" #will outpu 32
+
+
