fd82239adf6a2cef7e845fd3477be7a2e77feeb8
AnnahGerletti <agerletti@gmail.com>
2018-02-02 09:44:18 +1300

    puts,print,p

--- fundimentals.rb
+++ fundimentals.rb
@@ -1,4 +1,5 @@
 #Ruby is a language like JavaScript
+#Examples of Methods used to manipulate code
 
 # [puts - put string] outputs text to the screen like console.log, will alwasy have a line break after each line.
 puts "hello world"
@@ -6,5 +7,27 @@ puts "uauau"
 puts 5 #will output the number no double quotes 
 puts 5 + 4 #will output 9 the + concatinates the two numbers 
 puts "3" + "2" #will outpu 32
+puts "this is a
+line break"  #this will print the two line sentence 
 
 
+print "hello"
+print "world"
+#output "helloworld"
+
+#print puts everything on the same line no spaces
+#print is literal, will print whats between the ""
+
+print "hello "
+print "world"
+puts " "
+
+#adds a space within the "" or can add a third print with a single space
+
+#p method
+#the p method outputs code that is more literal to what it is, with cat you see the "" which indicates a string, With the sentence with a line break Ruby prints out \n = stands for a line break
+
+p "cat"
+
+p "this is a line
+break"
