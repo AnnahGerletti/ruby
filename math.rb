ca286b38bfd980f9c0169d567b2fbcb7b418bcd7
AnnahGerletti <agerletti@gmail.com>
2018-02-02 10:23:11 +1300

    all operators

--- math.rb
+++ math.rb
@@ -1,6 +1,37 @@
 #math in Ruby
 #uses opperators 
+# Dealing with numbers < 1 (.5) have to put the 0 before (0.5)
 
 #adding uses + 
+# + can also be used to combined things 
 
 p 3 + 4 # adds them together 
+
+#subtract uses - 
+
+p 3 - 2
+
+#multiplication uses * 
+
+p 3*4
+
+#divide uses / - but Ruby will return a full integer will not print remainders. 
+
+p 4/2
+p 562/5
+
+# to see the correct decimeal result need to make one of integers a decimeal
+
+p 562.0/5
+
+#exponents is expressed with **
+
+p 2 ** 4
+# two to the power of four
+
+
+#Modulous opperator will return the remainder of the opperation. 
+# Modular sign same in Ruby % 
+
+p 5 % 2 # 5 / 2 return 2.5 but module will output 1
+
