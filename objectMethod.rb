8680bed81059a9dcd199867f068f633d227d48df
AnnahGerletti <agerletti@gmail.com>
2018-02-11 14:25:42 +1300

    notes about object methods

--- objectMethod.rb
+++ objectMethod.rb
@@ -1,6 +1,8 @@
 =begin
 - Ruby methods use dot notation to stich together blocks of code.
 - Methods are just different actions taken on code. Length finds the length of something or next adds one.
+- whatever you imput into Ruby, it will output something.
+- ruby has to return an object (string, number, array). If it returns nothing it will return "nil"
 =end
 
 p "hello world".length 
@@ -14,3 +16,8 @@ p foo.upcase
 # this method adds one to the intiger. 
 p 20.next
 p -1.next + 7.next # the -1 takes 1 away from zero. then we add 7 to get 8
+
+#To include a variable with in a string use #{}
+
+name = "annah"
+p "my #{name} is, how are you"
