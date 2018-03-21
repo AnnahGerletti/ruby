7d928be111c5c1cc47a1e86a4b278a179d119d95
AnnahGerletti <agerletti@gmail.com>
2018-03-21 16:44:49 +1300

    first program Q/A

---
+++ ruby_practice.rb
@@ -0,0 +1,9 @@
+p "Hi, whats your name?"
+name = gets.chomp
+
+p "Great, thanks. How old are you?"
+age = gets.chomp.to_i
+
+p "Cool, so your name is #{name} and you are #{age} years old"
+
+# This is a question and answer program. 
